15d2e588f0130a08185cbd0103311d603d65be70
iaceene <yassineajagrou986@gmail.com>
2025-02-08 03:37:37 +0100

    expanding init

--- src/components/parser/expander/expander.c
+++ src/components/parser/expander/expander.c
@@ -6,7 +6,7 @@
 /*   By: yaajagro <yaajagro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 02:14:23 by yaajagro          #+#    #+#             */
-/*   Updated: 2025/02/08 03:06:01 by yaajagro         ###   ########.fr       */
+/*   Updated: 2025/02/08 03:35:59 by yaajagro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,13 @@ t_node  *expander(t_node *node, t_fake_env *head, t_cmd **cmd)
 {
     char    *value;
 
+    if (!node->next)
+        return (add_to_cmd(cmd, new_cmd(NULL)), NULL);
     value = node->next->value;
     while (head)
     {
-        if (ft_strstr(head->key, value))
+        if (ft_strncmp(head->key, value, ft_strlen(head->key)) == 0 && 
+			ft_strlen(head->key) == ft_strlen(value))
             break;
         head = head->next;
     }
