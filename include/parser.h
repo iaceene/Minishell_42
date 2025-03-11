df6bc8609b75f6c1d2adc0c634fcfbe6dd7a4ed1
iaceene <yassineajagrou986@gmail.com>
2025-03-11 23:39:16 +0000

    norm

--- include/parser.h
+++ include/parser.h
@@ -5,42 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yaajagro <yaajagro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/02 19:48:43 by iezzam            #+#    #+#             */
-/*   Updated: 2025/03/11 23:31:01 by yaajagro         ###   ########.fr       */
+/*   Created: 2025/02/02 19:48:43 by yaajagro          #+#    #+#             */
+/*   Updated: 2025/03/11 23:39:05 by yaajagro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
-#define PARSER_H
+# define PARSER_H
 
 # include "minishell.h"
 # include "stdbool.h"
 
-typedef struct s_node{
-	TokenType			type;
-	char				*value;
-	bool			    visit;
-	struct  s_node		*next;
-} t_node;
+typedef struct s_node
+{
+	TokenType		type;
+	char			*value;
+	bool			visit;
+	struct s_node	*next;
+}	t_node;
 
-typedef enum {
-    NORMAL,
-    IN_SQUOTE,
-    IN_DQUOTE
-} State;
+typedef enum e_state
+{
+	NORMAL,
+	IN_SQUOTE,
+	IN_DQUOTE
+}	t_state;
 
 typedef struct s_expand
 {
-	State			state;
+	t_state			state;
 	char			*val;
 	struct s_expand	*next;
 }	t_expand;
 
 typedef struct s_buffer
 {
-	char	*val;
-	struct s_buffer *next;
-}t_buffer;
+	char			*val;
+	struct s_buffer	*next;
+}	t_buffer;
 
 t_node		*lexer_init(const char *str);
 int			check_no_opned_pr(t_node *data);
@@ -48,7 +50,6 @@ int			find_it(char *s, char c);
 int			check_qoats(char *val);
 int			others_checker(t_node *data);
 int			check_redirction(t_node *data);
-int			check_no_opned_pr(t_node *data);
 int			valid_next(TokenType type);
 int			check_append(t_node *data);
 int			operator(char c);
@@ -57,21 +58,21 @@ void		add_to_list(t_node **head, t_node *new);
 t_node		*add_new_node(TokenType type, char *val);
 int			ft_isalnum(char c);
 char		*extract_word(char *s);
-void    	add_to_cmd(t_cmd **head, t_cmd *new);
-t_cmd   	*new_cmd(t_node *node);
+void		add_to_cmd(t_cmd **head, t_cmd *new);
+t_cmd		*new_cmd(t_node *node);
 char		*expander(t_node *node, t_env *head);
 t_cmd		*data_maker(t_node *head, t_env *env);
-t_expand	*new_expand(State state, char *val);
+t_expand	*new_expand(t_state state, char *val);
 void		add_expand(t_expand **head, t_expand *new);
 char		*expand_and_join(t_expand *head);
 char		*expand_this(char *str);
 char		*herdoc(t_env *env, char *exit);
-int	is_sperator(char c);
-char	*get_before(char *s);
-char	*get_after(char *s);
-char	*extract_name(char *str);
-void	handle_single_quote(char **input, t_expand **head, State *state);
-void	handle_double_quote(char **input, t_expand **head, State *state);
-void	handle_normal_text(char **input, t_expand **head, State state);
+int			is_sperator(char c);
+char		*get_before(char *s);
+char		*get_after(char *s);
+char		*extract_name(char *str);
+void		handle_single_quote(char **input, t_expand **head, t_state *state);
+void		handle_double_quote(char **input, t_expand **head, t_state *state);
+void		handle_normal_text(char **input, t_expand **head, t_state state);
 
 #endif
