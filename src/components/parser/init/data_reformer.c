144e4a58c1a8ad3406fa16824b9910910d1f3489
iaceene <yassineajagrou986@gmail.com>
2025-03-25 21:22:05 +0000

    reform the data

--- src/components/parser/init/data_reformer.c
+++ src/components/parser/init/data_reformer.c
@@ -6,7 +6,7 @@
 /*   By: yaajagro <yaajagro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 21:01:59 by yaajagro          #+#    #+#             */
-/*   Updated: 2025/03/25 21:18:48 by yaajagro         ###   ########.fr       */
+/*   Updated: 2025/03/25 21:21:40 by yaajagro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,28 @@ void	insert_cmd_before_redirs(t_cmd **head, t_cmd *cmd, t_cmd *redir)
 	{
 		cmd->next = redir;
 		*head = cmd;
+		return ;
 	}
+	before_redir = *head;
+	while (before_redir->next != redir)
+		before_redir = before_redir->next;
+	before_redir->next = cmd;
+	cmd->next = redir;
+}
+
+void	handle_command_node(t_cmd **head, t_cmd **current,
+		t_cmd **prev, t_cmd **first_redir)
+{
+	t_cmd	*cmd_node;
+
+	cmd_node = *current;
+	if (*prev != NULL)
+		(*prev)->next = cmd_node->next;
 	else
-	{
-		before_redir = *head;
-		while (before_redir->next != redir)
-			before_redir = before_redir->next;
-		before_redir->next = cmd;
-		cmd->next = redir;
-	}
+		*head = cmd_node->next;
+	insert_cmd_before_redirs(head, cmd_node, *first_redir);
+	*first_redir = NULL;
+	*current = cmd_node;
 }
 
 void	reforme_data(t_cmd **head)
@@ -36,12 +49,11 @@ void	reforme_data(t_cmd **head)
 	t_cmd	*current;
 	t_cmd	*prev;
 	t_cmd	*first_redir;
-	t_cmd	*cmd_node;
 
 	current = *head;
 	prev = NULL;
 	first_redir = NULL;
-	while (current != NULL)
+	while (current)
 	{
 		if (current->type == IN_FILE || current->type == OUT_FILE)
 		{
@@ -49,16 +61,7 @@ void	reforme_data(t_cmd **head)
 				first_redir = current;
 		}
 		else if (current->type == COMMAND && first_redir != NULL)
-		{
-			cmd_node = current;
-			if (prev != NULL)
-				prev->next = cmd_node->next;
-			else
-				*head = cmd_node->next;
-			insert_cmd_before_redirs(head, cmd_node, first_redir);
-			first_redir = NULL;
-			current = cmd_node;
-		}
+			handle_command_node(head, &current, &prev, &first_redir);
 		else
 			first_redir = NULL;
 		prev = current;
